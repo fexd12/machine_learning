34fb676a90b27e8af6015117e94f2a1015d1ae41
fexd12 <felipe.demelo@hotmail.com>
2020-11-21 20:26:48 -0300

    arrumando pre procesamento da base de dados census

--- pre_process/pre_process_census.py
+++ pre_process/pre_process_census.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from pandas.core.arrays import categorical
 from sklearn.preprocessing import LabelEncoder,OneHotEncoder,StandardScaler
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
@@ -24,11 +23,11 @@ def pre_process_census():
     previsor[:,9] = label_previsor.fit_transform(previsor[:,9])
     previsor[:,13] = label_previsor.fit_transform(previsor[:,13])
 
-    onehot = ColumnTransformer(transformers = [('OneHot',OneHotEncoder(),[1,3,5,6,7,8,9,13])],remainder = 'passthrough') # transformar em column dummy
-    previsor = onehot.fit_transform(previsor).toarray()
+    # onehot = ColumnTransformer(transformers = [('OneHot',OneHotEncoder(),[1,3,5,6,7,8,9,13])],remainder = 'passthrough') # transformar em column dummy
+    # previsor = onehot.fit_transform(previsor).toarray()
 
-    classe_label = LabelEncoder() # mudar variavel categorica 
-    classe = classe_label.fit_transform(classe)
+    # classe_label = LabelEncoder() # mudar variavel categorica 
+    # classe = classe_label.fit_transform(classe)
 
     scaler = StandardScaler() # fazer escalonamento de dados 
     previsor = scaler.fit_transform(previsor)
