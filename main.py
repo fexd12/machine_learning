2d4b206e3dd2d1d35f0598f55d1c1235415698ab
fexd12 <felipe.demelo@hotmail.com>
2020-11-21 20:27:34 -0300

    instaciando os modelos

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 from naive_bayes.nb_risco import nb_risco_main
 from naive_bayes.nb_credit import nb_credit_main
+from naive_bayes.nb_census import nb_census_main
+
+
+from arvores_decisao.ad_risco import ad_risco_main
+from arvores_decisao.ad_credit import ad_credit_main
+from arvores_decisao.ad_census import ad_census_main
+
+from random_forest.rf_risco import rf_risco_main
+from random_forest.rf_credit import rf_credit_main
+from random_forest.rf_census import rf_census_main
 
 # nb_risco_main()
-nb_credit_main()
+# nb_credit_main()
+# nb_census_main()
+
+# ad_risco_main()
+# ad_credit_main()
+# ad_census_main()
+
+rf_risco_main()
+rf_credit_main()
+rf_census_main()
